f12bd5f306471f3808869c630a89fd81bda8fb35
gobicycle <captonom@gmail.com>
2025-12-19 00:45:18 +0300

    optimize GetJettonInternalWithdrawalTasks request

--- db/db.go
+++ db/db.go
@@ -786,13 +786,15 @@ func (c *Connection) GetJettonInternalWithdrawalTasks(
 		SELECT deposit_address, MAX(lt) AS last_lt, jw.subwallet_id, jw.currency
 		FROM payments.external_incomes di
 			LEFT JOIN (
-			SELECT from_address, since_lt, finish_lt
+			SELECT iw1.from_address, iw1.since_lt, iw1.finish_lt
 			FROM payments.internal_withdrawals iw1
-			WHERE since_lt = (
-				SELECT MAX(iw2.since_lt)
-				FROM payments.internal_withdrawals iw2
-				WHERE iw2.from_address = iw1.from_address AND failed = false
-			)
+			JOIN (
+			    SELECT from_address, MAX(since_lt) AS max_since_lt
+			    FROM payments.internal_withdrawals
+			    WHERE failed = false 
+			    GROUP BY from_address 
+			) iw2 ON iw2.from_address = iw1.from_address AND iw2.max_since_lt = iw1.since_lt
+			WHERE iw1.failed = false
 		) as iw3 ON from_address = deposit_address
 		JOIN payments.jetton_wallets jw ON di.deposit_address = jw.address
 		LEFT JOIN payments.ton_wallets tw ON jw.subwallet_id = tw.subwallet_id
--- deploy/manual_migrations/0.5.x-0.6.3.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS internal_withdrawals_failed_from_address_index
-    ON payments.internal_withdrawals (failed, from_address);
